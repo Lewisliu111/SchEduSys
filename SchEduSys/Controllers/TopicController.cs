408511f6833c2ab73cec02ce00318b732e66ef4b
ChenWenao <747273894@qq.com>
2020-03-17 22:48:58 +0800

    添加topic方法

--- SchEduSys/Controllers/TopicController.cs
+++ SchEduSys/Controllers/TopicController.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SchEduSys.Models;
+using MySql.Data.EntityFramework;
 
 namespace SchEduSys.Controllers
 {
     public class TopicController : Controller
     {
         public SchEduSysEntities schEduSysEntities = new SchEduSysEntities();
-        // GET: Topic
+       
+
+        //添加一个课程类型，例如：计算机类
         public bool AddATopic(String tName)
         {
             coursetopic newtopic = new coursetopic()
@@ -25,6 +28,19 @@ namespace SchEduSys.Controllers
             {
                 return false;
             }
+            return true;
+        }
+
+        //删除一个课程类型，例如：数学类
+        public bool DropATopic(String dName)
+        {
+            coursetopic dTopic = schEduSysEntities.coursetopic.FirstOrDefault(m => m.topicName == dName);
+            if (dTopic == null)
+            {
+                return false;
+            }
+            schEduSysEntities.coursetopic.Remove(dTopic);
+            schEduSysEntities.SaveChanges();
             return true;
         }
     }
