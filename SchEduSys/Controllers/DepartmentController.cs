0040254eeab70cc95ce9574e027af5bf20f31e53
ChenWenao <747273894@qq.com>
2020-03-17 21:42:52 +0800

    更新了models

--- SchEduSys/Controllers/DepartmentController.cs
+++ SchEduSys/Controllers/DepartmentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SchEduSys.Models;
 
 namespace SchEduSys.Controllers
 {
--- SchEduSys/Controllers/TopicController.cs
+++ SchEduSys/Controllers/TopicController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SchEduSys.Models;
 
 namespace SchEduSys.Controllers
 {
     public class TopicController : Controller
     {
+        public SchEduSysEntities schEduSysEntities = new SchEduSysEntities();
         // GET: Topic
-        public ActionResult Index()
+        public bool AddATopic(String tName)
         {
-            return View();
+            coursetopic newtopic = new coursetopic()
+            {
+                topicName = tName
+            };
+            try
+            {
+                schEduSysEntities.coursetopic.Add(newtopic);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
